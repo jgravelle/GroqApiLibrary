85fed2f0c4035862d26551482c16685b4364bb13
MarcusCazzola <marcus.cazzola@gmail.com>
2024-03-17 15:42:20 +0100

    no class Program

--- Program.cs
+++ Program.cs
@@ -1,35 +1,29 @@
 ﻿using GroqApiLibrary;
 using Newtonsoft.Json.Linq;
 
-class Program
+string key = "xxxxxxxxxxx";
+GroqApiLibrary.GroqApiClient groqApi = new GroqApiLibrary.GroqApiClient(key);
+
+JObject request = new JObject
 {
-    static void Main(string[] args)
+    ["model"] = "mixtral-8x7b-32768",
+    ["messages"] = new JArray
     {
-        string key = "xxxxxxxxxxx";
-        GroqApiLibrary.GroqApiClient groqApi = new GroqApiLibrary.GroqApiClient(key);
-
-        JObject request = new JObject
+        new JObject
         {
-            ["model"] = "mixtral-8x7b-32768",
-            ["messages"] = new JArray
-            {
-                new JObject
-                {
-                    ["role"] = "system",
-                    ["content"] = "You are a chatbot that holds every answer to every question"
-                },
-                new JObject
-                {
-                    ["role"] = "user",
-                    ["content"] = "What is the meaning of life?"
-                }
-            }
-        };
-
-        JObject result = groqApi.CreateChatCompletionAsync(request).Result;
-
-        string? response = result?["choices"]?[0]?["message"]?["content"]?.ToString();
-        Console.WriteLine(response);
-        Console.ReadLine();
+            ["role"] = "system",
+            ["content"] = "You are a chatbot that holds every answer to every question"
+        },
+        new JObject
+        {
+            ["role"] = "user",
+            ["content"] = "What is the meaning of life?"
+        }
     }
-}
+};
+
+JObject result = await groqApi.CreateChatCompletionAsync(request);
+
+string? response = result?["choices"]?[0]?["message"]?["content"]?.ToString();
+Console.WriteLine(response);
+Console.ReadLine();
